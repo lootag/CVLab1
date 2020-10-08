2b8065d58a88143c27bb722a2ac8893e7eb4cb62
Alex <luigi.taglialatela1996@gmail.com>
2020-10-08 17:15:35 +0200

    first commit

---
+++ Program.py
@@ -0,0 +1,10 @@
+class Program:
+    def __init__(self):
+        pass
+    def Main(self):
+        print("hello world")
+
+program = Program()
+
+if __name__ == "__main__":
+    program.Main()
---
+++ test.py
@@ -0,0 +1,5 @@
+class Program:
+    def __init__(self):
+        pass
+    def hello():
+        print("hello world")
